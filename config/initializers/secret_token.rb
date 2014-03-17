9c90d091203ec67ae5621cf783c1fc9e5c7f2b53
Patrick Turley [pturley] <patrick.turley@gmail.com>
2014-03-17 12:56:50 -0500

    Adding initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EatNRUn::Application.config.secret_key_base = 'ec336d5f0782ac9a417194f03924600bfa246c5b8e743e6d3373a0a1cc5214bcab533c2e96010b993f9e1afacb7b456b256dc45d8ebcd4240de54a889009c493'
---
+++ vendor/bundle/gems/atomic-1.1.16/ext/siteconf20140317-76574-ofse7f.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140317-76574-19lgrhk"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
